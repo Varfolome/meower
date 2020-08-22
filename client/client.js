5e7528074d4ac0e97cfc59320baf4759886e23fc
Tomati <dopedup@mail.ru>
2020-08-22 21:05:02 +0300

    Added server

--- client/client.js
+++ client/client.js
@@ -1,10 +1,31 @@
 console.log("Hello from client");
 
 const form = document.querySelector('form');
+const loading = document.querySelector('.loading');
+const API_URL = 'http://localhost:5000/mews';
+
+loading.style.display = 'none';
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const formData = new FromData(form);
+  const formData = new FormData(form);
   const name = formData.get('name');
   const content = formData.get('content');
+
+  const mew = {
+    name,
+    content
+  };
+
+  form.style.display = 'none';
+  loading.style.display = '';
+
+  console.log(mew);
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(mew),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
 });
