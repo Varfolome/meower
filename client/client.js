0fcffec79396d8104ce8837bfbb41bd9898bb0f0
Tomati <dopedup@mail.ru>
2020-08-25 22:05:25 +0300

    Doesn't work because of non matching of json and mongoDB type representation

--- client/client.js
+++ client/client.js
@@ -6,6 +6,8 @@ const API_URL = 'http://localhost:5000/mews';
 
 loading.style.display = 'none';
 
+listAllMews();
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -20,12 +22,28 @@ form.addEventListener('submit', (event) => {
   form.style.display = 'none';
   loading.style.display = '';
 
-  console.log(mew);
   fetch(API_URL, {
     method: 'POST',
     body: JSON.stringify(mew),
     headers: {
       'content-type': 'application/json'
     }
-  });
+  }).then(response => response.json)
+    .then(createdMew => {
+      console.log(createdMew);
+      form.reset();
+      loading.style.display = 'none';
+      form.style.display = '';
+    });
 });
+
+function listAllMews() {
+  fetch(API_URL)
+    .then(response => response.json)
+    .then(mews => {
+      console.log(mews);
+      mews.forEach(mew => {
+        console.log(mew);
+      });
+    });
+}
--- server/index.js
+++ server/index.js
@@ -1,21 +1,66 @@
 const express = require('express');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+
+const uri = "mongodb+srv://tsimafei:dataformeower123@cluster0.cqiwv.mongodb.net/startdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
-  res.json({
-    message: 'Meower! :)'
+client.connect(err => {
+
+  const mews = client.db('meower').collection('mews');
+
+  app.get('/', (req,res) => {
+    res.json({
+      message: 'Meower! :)'
+    });
   });
-});
 
-app.post('/mews', (req,res) => {
-  console.log(req.body);
-});
+  app.get('/mews', (req,res) => {
+    res.json(mews);
+  });
 
-app.listen(5000, () => {
-  console.log('Listening');
+
+  function isValidMew(mew){
+    return mew.name && mew.name.toString().trim() !== '' &&
+      mew.content && mew.content.toString().trim() !== '';
+  }
+
+  app.post('/mews', (req,res) => {
+    if(isValidMew(req.body)) {
+      const mew = {
+        name: req.body.name.toString(),
+        content: req.body.content.toString(),
+        created: new Date()
+      };
+
+        mews
+          .insertOne(mew)
+          .then(createdMew => {
+            res.json(createdMew);
+          });
+
+      console.log(mew);
+    } else {
+      res.status(422);
+      res.json({
+        message: 'Hey! Its empty!'
+      });
+    }
+  });
+
+  app.listen(5000, () => {
+    console.log('Listening');
+  });
+
+//client.close();
+
 });
